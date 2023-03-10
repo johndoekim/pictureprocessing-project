a6f7fc5c9d2afa5860ba1d3786dc0476b09baf56
junoh <amok0316@gmail.com>
2023-03-10 10:54:09 +0900

    Master commit2
    그레이스케일 효과 추가

--- image processing.py
+++ image processing.py
@@ -3,9 +3,24 @@
 
 import cv2
 
+# 영상 소스 열기
 src1=cv2.imread('/Users/junpyooh/images/111.jpg')
+
+### 영상 처리 알고리즘을 구현###
+
+
+dst1 = cv2.cvtColor(src1,cv2.COLOR_BGR2GRAY)
+
+
+
+# 영상 디스플레이
 
 cv2.imshow('src1',src1)
 
+cv2.imshow('dst1', dst1)
+
+
+
+# 마무리
 cv2.waitKey(0)
 cv2.destroyAllWindows()
